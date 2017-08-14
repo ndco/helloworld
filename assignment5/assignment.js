33c4ccec6663466a8d86e9ef2d7b16a3f99ad75f
Andy Seo <sbki99@gmail.com>
2017-08-14 19:07:26 -0400

    9th Commit: modified prize to array in assignment.js

--- assignment5/assignment.js
+++ assignment5/assignment.js
@@ -1,4 +1,4 @@
-//Create a script with two variables, each assigned to a number. Add them together and output the result to the console. Now do the same with two strings.
+// //Create a script with two variables, each assigned to a number. Add them together and output the result to the console. Now do the same with two strings.
 var a = 10
 var b = 7
 var c = a + b
@@ -6,7 +6,7 @@ console.log(a+b);
 console.log(c);
 
 
-//Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
+// //Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays to the console.
 var myInterest = 
 	[
 		["skiing", "basketball", "soccer"],
@@ -18,8 +18,9 @@ console.log(myInterest);
 console.log(myInterest[0] [0] + " and " + myInterest[2] [1]);
 
 
-//Write a script that checks if a variable is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
-// var num = 99
+// //Write a script that checks if a variable is less than 100. If it is, alert the user that their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
+
+var num = 99
 
 if (num < 100) {
 	alert(num + " is less than 100");
@@ -36,7 +37,7 @@ if (num < 100) {
 
 
 
-// Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it after it has been declared.
+// // Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it after it has been declared.
 
 function alertUserName(user) {
 	return alert("You have entered username as " + user);
@@ -54,15 +55,15 @@ function openDoor(door) {
 	}	
 
 	// Check if the input is in range of 1 - 3
-	if (door > 4 || door <= 0) {
+	if (door >= 4 || door <= 0) {
 		return alert("You have entered invalid door #");
 	}
 
 	// Reward for opening correct doors
-	var	prize = door * 100;
+	prize = ["Car", "iPod", "Cash"];
 	while (door < 4) {
-		return alert("You have earned $" + prize);
+		return alert("You have earned " + prize[door -1]);
 	} 
 }
 
-openDoor("0");
+openDoor(0);
